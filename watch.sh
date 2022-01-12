da72e4b2fe7f976c6028e52a4a6d9d2c33575b72
William Cotton <williamcotton@gmail.com>
2022-01-12 13:24:30 -0600

    clean up awk in watch.sh

--- watch.sh
+++ watch.sh
@@ -1,7 +1,7 @@
 # https://learnersguide.wordpress.com/2015/02/22/275/
 start_cmd="build/$1 &"
 ps | grep "build/$1$" | awk -v app=$1 '{
-    if($1!="") {
+    if ($1 != "") {
         print "\nRestarting process: "$1;
         system("kill " $1);
         system("make $app");
