f462bc473dd88d79fea593c640e484ab8e032596
William Cotton <williamcotton@gmail.com>
2022-01-23 09:01:04 -0600

    add httpfuzz audit

---
+++ audit/fuzz_post.sh
@@ -0,0 +1,13 @@
+# ../fuzzdb/attack/all-attacks/all-attacks-unix.txt
+# ../fuzzdb/attack/format-strings/format-strings.txt
+
+httpfuzz \
+   --wordlist ../fuzzdb/attack/format-strings/format-strings.txt \
+   --seed-request fuzz/POST.request \
+   --target-header User-Agent \
+   --target-header Host \
+   --delay-ms 50 \
+   --target-header Pragma \
+   --skip-cert-verify \
+   --target-param fuzz \
+   --dirbuster
