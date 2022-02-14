b7dceae70f7f4c4e9a5a76dec06e3f17d609a81e
William Cotton <williamcotton@gmail.com>
2022-02-14 10:20:46 -0600

    rename and fix embed script

---
+++ scripts/embed.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+ls public/* | xargs -I % xxd -i % > $1
+ls views/* | xargs -I % xxd -i % >> $1
+
+cat $1 | grep 'unsigned char' | awk '{print $3}' | sed 's/[\[\]]*//g' | awk '{printf("%s, ", $1)}' | sed 's/, $//' | awk '{
+  print "unsigned char *embeddedFilesData[] = {" $0 "};"
+}'
+
+cat $1 | grep 'unsigned int' | awk '{print $5}' | sed 's/[;]*//g' | awk '{printf("%s, ", $1)}' | sed 's/, $//' | awk '{
+  print "int embeddedFilesLengths[] = {" $0 "};"
+}'
+
+cat $1 | grep 'unsigned char' | awk '{print $3}' | sed 's/[\[\]]*//g' | awk '{printf("\"%s\", ", $1)}' | sed 's/, $//' | awk '{
+  print "char *embeddedFilesNames[] = {" $0 "};"
+}'
+
+cat $1 | grep 'unsigned int' | wc -l | awk '{
+  print "const int embeddedFilesCount = " $0 ";"
+}'
+
+cat <<EOF
+embedded_files_data_t embeddedFiles = {embeddedFilesData, embeddedFilesLengths, embeddedFilesNames, embeddedFilesCount};
+EOF
