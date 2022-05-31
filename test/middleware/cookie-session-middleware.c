02fd41ef2fce85e52fc47da84b7d4ef931a24c13
William Cotton <williamcotton@gmail.com>
2022-05-31 09:47:53 -0500

    add refactor notes

--- test/middleware/cookie-session-middleware.c
+++ test/middleware/cookie-session-middleware.c
@@ -7,6 +7,9 @@
 
 void cookieSessionMiddlewareTests(tape_t *t) {
   t->test("cookie session middleware", ^(UNUSED tape_t *t) {
+    // TODO: figure out why this is breaking on GH actions on darwin
+    // ... it runs fine on darwin locally...
+    // bigger picture: TODO: use curl instead of system(curl)
     // t->strEqual("set", t->get("/cookie-session/set"), "ok");
     // t->strEqual("get", t->get("/cookie-session/get"), "{\"title\":\"test\"}");
   });
