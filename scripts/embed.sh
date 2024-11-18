2d286af7246887328298fe513b139a19a83b1c2f
William Cotton <williamc@eurekasoft.com>
2024-11-18 08:11:31 -0600

    fix embed.sh

--- scripts/embed.sh
+++ scripts/embed.sh
@@ -1,26 +1,50 @@
 #!/usr/bin/env bash
 
-# TODO: make public and view dirs arguments
+# Check for correct usage
+if [ "$#" -lt 2 ]; then
+  echo "Usage: $0 <output_file> <dir1> [dir2] ... [dirN]"
+  exit 1
+fi
 
-ls $2 | xargs -I % xxd -i % > $1
-ls $3 | xargs -I % xxd -i % >> $1
+output_file=$1
+shift  # Shift the arguments to process directories
+directories=("$@")
 
-cat $1 | grep 'unsigned char' | awk '{print $3}' | sed 's/[\[\]]*//g' | awk '{printf("%s, ", $1)}' | sed 's/, $//' | awk '{
-  print "unsigned char *embeddedFilesData[] = {" $0 "};"
-}'
+# Clear or create the output file
+> "$output_file"
 
-cat $1 | grep 'unsigned int' | awk '{print $5}' | sed 's/[;]*//g' | awk '{printf("%s, ", $1)}' | sed 's/, $//' | awk '{
-  print "int embeddedFilesLengths[] = {" $0 "};"
-}'
+# Function to process files in directories
+process_directory() {
+  local dir=$1
+  if [ -d "$dir" ]; then
+    for file in "$dir"/*; do
+      if [ -f "$file" ]; then
+        xxd -i "$file" >> "$output_file"
+      fi
+    done
+  else
+    echo "Warning: Directory '$dir' does not exist or is not a directory. Skipping."
+  fi
+}
 
-cat $1 | grep 'unsigned char' | awk '{print $3}' | sed 's/[\[\]]*//g' | awk '{printf("\"%s\", ", $1)}' | sed 's/, $//' | awk '{
-  print "char *embeddedFilesNames[] = {" $0 "};"
-}'
+# Process each directory
+for dir in "${directories[@]}"; do
+  process_directory "$dir"
+done
 
-cat $1 | grep 'unsigned int' | wc -l | awk '{
-  print "const int embeddedFilesCount = " $0 ";"
-}'
+# Generate metadata
+embedded_files_data=$(grep 'unsigned char' "$output_file" | awk '{print $3}' | sed 's/[\[\]]*//g' | awk '{printf("%s, ", $1)}' | sed 's/, $//')
+embedded_files_lengths=$(grep 'unsigned int' "$output_file" | awk '{print $5}' | sed 's/[;]*//g' | awk '{printf("%s, ", $1)}' | sed 's/, $//')
+embedded_files_names=$(grep 'unsigned char' "$output_file" | awk '{print $3}' | sed 's/[\[\]]*//g' | awk '{printf("\"%s\", ", $1)}' | sed 's/, $//')
+embedded_files_count=$(grep 'unsigned int' "$output_file" | wc -l)
 
-cat <<EOF
-embedded_files_data_t embeddedFiles = {embeddedFilesData, embeddedFilesLengths, embeddedFilesNames, embeddedFilesCount};
+# Append the metadata to the output file
+cat <<EOF >> "$output_file"
+
+unsigned char *embeddedFilesData[] = { $embedded_files_data };
+int embeddedFilesLengths[] = { $embedded_files_lengths };
+char *embeddedFilesNames[] = { $embedded_files_names };
+const int embeddedFilesCount = $embedded_files_count;
+
+embedded_files_data_t embeddedFiles = { embeddedFilesData, embeddedFilesLengths, embeddedFilesNames, embeddedFilesCount };
 EOF
